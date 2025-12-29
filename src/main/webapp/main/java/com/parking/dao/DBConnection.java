939be3aceafe528d35050831a35b522cd1e88a0f
Priyanshu Prasad <priyanshuprasad2517@gmail.com>
2025-12-29 15:11:13 +0530

    Update DBConnection to load database config from properties
    Refactored DBConnection to use properties file for configuration.

--- src/main/webapp/main/java/com/parking/dao/DBConnection.java
+++ src/main/webapp/main/java/com/parking/dao/DBConnection.java
@@ -2,14 +2,29 @@ package com.parking.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+import java.io.InputStream;
 
 public class DBConnection {
-    public static Connection getConnection() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/parkingdb",
-            "root",
-            "password"
-        );
+
+    public static Connection getConnection() {
+        try {
+            Properties props = new Properties();
+            InputStream is = DBConnection.class
+                    .getClassLoader()
+                    .getResourceAsStream("db.properties");
+            props.load(is);
+
+            Class.forName(props.getProperty("driver"));
+
+            return DriverManager.getConnection(
+                    props.getProperty("url"),
+                    props.getProperty("username"),
+                    props.getProperty("password")
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
